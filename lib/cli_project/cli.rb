d5b53fefe0cabd68b3c5f198141768784d361fd2
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-02 21:03:14 +0000

    link to api

--- lib/cli_project/cli.rb
+++ lib/cli_project/cli.rb
@@ -5,13 +5,15 @@ module CliProject
     
     
       #greeting
-      #instructions - enter an ingredient(s)
+      #instructions - enter an ingredient(s), keywords, 
       #return a list of recipes including that ingredient
         #- Alternate idea: search the returned recipes by other ingredients
         #-show filtered list
+          #-optional: search by tags like vegetarian, dessert, etc
       #"what recipe do you want to see?" 
       #show full recipe 
       #"do you want to search again?"
+      #return error message when input is not recognized/no match
       #"thanks for using my CLI and happy cooking!"
       
       
